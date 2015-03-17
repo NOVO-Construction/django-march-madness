f46f3aec2e2de25400cfdf12441c5482352672e8
Sam Kuehn <samkuehn@gmail.com>
2015-03-16 20:01:25 -0600

    add project js

--- django-march-madness/static/js/project.js
+++ django-march-madness/static/js/project.js
@@ -1,1 +1,39 @@
-/* Project specific Javascript goes here. */
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+  var method;
+  var noop = function() {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+})();
+
+
+(function($, window, document, undefined) {
+  function csrfSafeMethod(method) {
+      // these HTTP methods do not require CSRF protection
+      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type)) {
+        var csrftoken = $.cookie('csrftoken');
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    }
+  });
+})(jQuery, window, document);
