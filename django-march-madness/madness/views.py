752e0c8e0870325c2a459eda5c170be50369dd9d
Sam Kuehn <samkuehn@gmail.com>
2015-03-16 20:33:39 -0600

    add post handler

--- django-march-madness/madness/views.py
+++ django-march-madness/madness/views.py
@@ -1,13 +1,21 @@
-from braces.views import LoginRequiredMixin
+import logging
+
+from braces.views import JsonRequestResponseMixin, LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 from .models import Bracket
 
+log = logging.getLogger(__name__)
 
-class EnterPicksView(LoginRequiredMixin, TemplateView):
+
+class EnterPicksView(LoginRequiredMixin, JsonRequestResponseMixin, TemplateView):
     template_name = 'brackets/entry.html'
 
     def get_context_data(self, **kwargs):
         context = super(EnterPicksView, self).get_context_data(**kwargs)
         context['bracket'] = Bracket.objects.filter(year=2015)
         return context
+
+    def post(self, request, *args, **kwargs):
+        log.debug(self.request_json)
+        return self.render_json_response({'message': ('Your pick has been made')})
