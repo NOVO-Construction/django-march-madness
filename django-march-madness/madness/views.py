40b661d6ae875d9de326a422fcd41ddb02faa931
Sam Kuehn <samkuehn@gmail.com>
2015-03-16 19:22:46 -0600

    clean up code

--- django-march-madness/madness/views.py
+++ django-march-madness/madness/views.py
@@ -1,10 +1,11 @@
 from django.views.generic.base import TemplateView
 from .models import Bracket
 
-class EnterPicksView(TemplateView):
-	template_name = 'brackets/entry.html'
 
-	def get_context_data(self, **kwargs):
-		context = super(EnterPicksView, self).get_context_data(**kwargs)
-		context['bracket'] = Bracket.objects.filter(year=2015)
-		return context
+class EnterPicksView(TemplateView):
+    template_name = 'brackets/entry.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EnterPicksView, self).get_context_data(**kwargs)
+        context['bracket'] = Bracket.objects.filter(year=2015)
+        return context
