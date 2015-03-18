489a909131fbda818bfc7704938905d648e9743a
Sam Kuehn <samkuehn@gmail.com>
2015-03-17 20:31:33 -0600

    add titles in js

--- django-march-madness/templates/brackets/entry.js
+++ django-march-madness/templates/brackets/entry.js
@@ -18,6 +18,7 @@ MM.EntryPickView = Backbone.View.extend({
   },
   render: function () {
     this.$el.text(this.model.get('pick_team_display'));
+    this.$el.prop('title', this.model.get('pick_team_display'));
     return this;
   },
   click: function (event) {
