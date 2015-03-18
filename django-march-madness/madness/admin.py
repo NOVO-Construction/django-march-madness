058aeca4e4ebddec2b261f68c0da7ca808961682
Sam Kuehn <samkuehn@gmail.com>
2015-03-18 09:40:14 -0600

    beef up admin

--- django-march-madness/madness/admin.py
+++ django-march-madness/madness/admin.py
@@ -1,33 +1,45 @@
 from django.contrib import admin
+from django.db.models import Count
 
-from madness.models import Bracket, Entry, EntryPick, Game, Region, Team
+from core.admin import RelatedFieldAdmin
+
+from . import models
 
 
-@admin.register(Region)
+@admin.register(models.Region)
 class RegionAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Team)
+@admin.register(models.Team)
 class TeamAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Bracket)
+@admin.register(models.Bracket)
 class BracketAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Game)
+@admin.register(models.Game)
 class GameAdmin(admin.ModelAdmin):
     pass
 
 
-@admin.register(Entry)
-class EntryAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.Entry)
+class EntryAdmin(RelatedFieldAdmin):
+    list_display = ('name', 'user__first_name', 'user__last_name', 'user__username', 'entrypick_count')
+    list_select_related = ('user',)
+
+    def queryset(self, request):
+        return models.Entry.objects.annotate(entrypick_count=Count('entrypick'))
+
+    def entrypick_count(self, inst):
+        return inst.entrypick_count
+    entrypick_count.admin_order_field = 'entrypick_count'
 
 
-@admin.register(EntryPick)
-class EntryPickAdmin(admin.ModelAdmin):
-    pass
+@admin.register(models.EntryPick)
+class EntryPickAdmin(RelatedFieldAdmin):
+    list_display = ('pick', 'entry__name', 'entry__user__first_name', 'entry__user__last_name', 'entry__user__username')
+    list_select_related = ('entry__user', 'pick__team')
