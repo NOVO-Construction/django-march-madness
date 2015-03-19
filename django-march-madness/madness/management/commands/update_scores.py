d78f30adbb348b50c92aadb58b3c6dd84d62fda1
Colin Stoner <colinstoner@gmail.com>
2015-03-19 10:28:57 -0600

    Leave this alone for now, incomplete.

--- django-march-madness/madness/management/commands/update_scores.py
+++ django-march-madness/madness/management/commands/update_scores.py
@@ -19,7 +19,12 @@ class Command(NoArgsCommand):
 		r = requests.get(url)
 		c = urllib.unquote(r.text).decode(r.encoding)
 		for g in Game.objects.all():
-			win_1 = re.compile(r'%s \d+   \^%s \d+' % (g.team_2.team.espn_id, g.team_1.team.espn_id))
-			win_2 = re.compile(r'\^%s \d+   %s \d+' % (g.team_2.team.espn_id, g.team_1.team.espn_id))
-			print win_1.pattern
-			print win_2.pattern
+			regexp = re.compile(r'%s (.*?) %s' % (g.team_2.team.espn_id, g.team_1.team.espn_id))
+			if regexp.search(c) is not None:
+				print regexp.pattern
+			else:
+				print "No game today"
+			# win_1 = re.compile(r'%s \d+   \^%s \d+' % (g.team_2.team.espn_id, g.team_1.team.espn_id))
+			# win_2 = re.compile(r'\^%s \d+   %s \d+' % (g.team_2.team.espn_id, g.team_1.team.espn_id))
+			# print win_1.pattern
+			# print win_2.pattern
