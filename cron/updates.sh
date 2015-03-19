bcf2d8b129e57a54a6f84a30eb3a6b60e4999a41
Colin Stoner <colinstoner@gmail.com>
2015-03-19 16:12:16 -0600

    Score, standings, and possible points update.

---
+++ cron/updates.sh
@@ -0,0 +1,4 @@
+# get scores, update standings, update possible points every 5 min
+/home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_scores
+/home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_standings
+/home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_possible_points
