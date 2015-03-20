0e440d3c749a1cddfb9fd45ad667715a61b81bbd
Colin Stoner <colinstoner@gmail.com>
2015-03-19 19:01:42 -0600

    Initiate.

--- cron/updates.sh
+++ cron/updates.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # get scores, update standings, update possible points every 5 min
 /home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_scores
 /home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_standings
