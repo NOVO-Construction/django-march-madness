c88564f19f8f6b1c8ebc412252441ba07ea83233
Colin Stoner <colinstoner@gmail.com>
2015-03-19 19:00:54 -0600

    Revert "Initiate."
    This reverts commit 6539ae3290a34f745758db37d8b6be60b59f304a.

--- cron/updates.sh
+++ cron/updates.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # get scores, update standings, update possible points every 5 min
 /home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_scores
 /home/ec2-user/.virtualenvs/madness/bin/python /home/ec2-user/madness/django-march-madness/manage.py update_standings
