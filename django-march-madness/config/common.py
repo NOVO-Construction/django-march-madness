dd34b660be0d2a298a9c6ce0499d8ee140d66799
Sam Kuehn <samkuehn@gmail.com>
2016-03-17 10:17:00 -0600

    lock brackets

--- django-march-madness/config/common.py
+++ django-march-madness/config/common.py
@@ -291,4 +291,4 @@ class Common(Configuration):
         cls.DATABASES['default']['ATOMIC_REQUESTS'] = True
 
     # Your common stuff: Below this line define 3rd party library settings
-    LOCK_BRACKETS = False
+    LOCK_BRACKETS = True
